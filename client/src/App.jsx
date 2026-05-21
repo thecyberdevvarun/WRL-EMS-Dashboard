3b42360ed88bcb3edf37eef65fe65686317a8fe4
thecyberdevvarun <thecyberdevvarun@gmail.com>
2026-05-21 13:33:53 +0530

    react tailwindcss setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <section>
+        <h1 className="text-red-700">Hello</h1>
+      </section>
+    </>
+  );
+}
+
+export default App;
